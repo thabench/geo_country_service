ca792e61fdda24e82def1c8ef4165e72ee63024b
Benas Jasikonis <benasjasikonis@carvertical.com>
2025-06-13 10:21:27 +0300

    move from fastapi

---
+++ handler.py
@@ -0,0 +1,39 @@
+import orjson
+import os
+from shapely.geometry import shape
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "world_countries.geojson")
+
+
+def get_country_code(latitude: float, longitude: float) -> dict[str, str | None]:
+    result = {"message": "", "result": None}
+    coord_string = f"latitude: {latitude}, longitude: {longitude}"
+    if not latitude or not longitude:
+        result["message"] = f"Invalid coordinates. lat: {latitude}, lon: {longitude}"
+        return result
+
+    alpha3_code = ""
+    with open(DATA_PATH, "rb") as f:
+        countries = orjson.loads(f.read())
+
+    mapped_country_shapes = (
+        {idx: {"shape": shape(feature["geometry"]), "countryCode": feature["properties"].get("iso_a3")}
+         for idx, feature
+         in enumerate(countries["features"])}
+    )
+    coordinates_point = shape(context={"type": "Point", "coordinates": [longitude, latitude]})
+
+    for key, value in mapped_country_shapes.items():
+        if coordinates_point.intersects(value["shape"]):
+            alpha3_code = value.get("countryCode")
+            break
+
+    if not alpha3_code:
+        result["message"] = f"Country code not found for coordinates: {coord_string}"
+        return result
+
+    result["message"] = "success"
+    result["result"] = f"{alpha3_code}"
+    return result
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,24 +1,19 @@
-import api
-import json
+import handler
 
 
-def test_api_ok_result():
-    response = api.get_country_code(latitude=54.769350220741586, longitude=25.319456079720805)
-    assert response.status_code == 200
-    assert json.loads(response.body).get("result") == "LTU"
+def test_ok_result():
+    result = handler.get_country_code(latitude=54.769350220741586, longitude=25.319456079720805)
+    assert result["message"] == "success"
+    assert result["result"] == "LTU"
 
 
-def test_api_no_country():
-    response = api.get_country_code(latitude=0.1, longitude=0.1)
-    response_json = json.loads(response.body)
-    assert response.status_code == 400
-    assert not response_json.get("result")
-    assert response_json.get("message") == "Country code not found for coordinates: latitude: 0.1, longitude: 0.1"
+def test_no_country():
+    result = handler.get_country_code(latitude=0.1, longitude=0.1)
+    assert not result.get("result")
+    assert result.get("message") == "Country code not found for coordinates: latitude: 0.1, longitude: 0.1"
 
 
-def test_api_bad_payload():
-    response = api.get_country_code(latitude=100, longitude=None)
-    assert response.status_code == 400
-    response_json = json.loads(response.body)
-    assert not response_json.get("result")
-    assert response_json.get("message") == "Bad coordinates: latitude: 100, longitude: None"
+def test_bad_payload():
+    result = handler.get_country_code(latitude=100, longitude=None)
+    assert not result.get("result")
+    assert result.get("message") == "Invalid coordinates. lat: 100, lon: None"
